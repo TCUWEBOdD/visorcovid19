ff1330d52cce77cd1184d7db6ed3d58a01f8cfe4
sebas3552 <cruzchavarria925@hotmail.com>
2021-08-21 19:23:09 -0600

    COV-37: Regeneración de documentación html

--- crawler/vacunas/vacunas/spiders/vacunasspider.py
+++ crawler/vacunas/vacunas/spiders/vacunasspider.py
@@ -1,13 +1,43 @@
 import scrapy
 from vacunas.items import VacunasItem
 
+
 class VacunasSpider(scrapy.Spider):
-    name = 'vacunas'
-    start_urls = ['https://www.ccss.sa.cr/web/coronavirus/vacunacion']
+    """
+    Clase que representa el spider que recopila los datos de vacunación desde la página de la [CCSS](https://www.ccss.sa.cr/web/coronavirus/vacunacion)
+    en el proceso diario de carga de datos.
+    """
+
+    name = "vacunas"
+    start_urls = ["https://www.ccss.sa.cr/web/coronavirus/vacunacion"]
 
     def parse(self, response):
-        item_primera_dosis = response.css('.cifra1.bg3.counter::text').getall()[0].replace(',', '')
-        item_segunda_dosis = response.css('.cifra1.bg4.counter::text').getall()[0].replace(',', '')
-        item_fecha = response.css('.actualiza.d-flex.justify-content-between.flex-column.flex-md-row p::text').getall()[1].strip()
-        vacunasItem = VacunasItem(primera_dosis = item_primera_dosis, segunda_dosis = item_segunda_dosis, fecha = item_fecha)
+        """
+        Lee la página web y obtiene los datos de cantidad de vacunas aplicadas (primera y segunda dosis) y la fecha de los datos.
+        Luego el resto del pipeline continua con el procesamiento, guardando los datos recopilados en la base de datos, en la tabla ```datos_pais```.
+
+        Parámetros
+        ----------
+        response
+            Respuesta que carga el contenido de la página web según la URL dada en la configuración del spider.
+        """
+
+        item_primera_dosis = (
+            response.css(".cifra1.bg3.counter::text").getall()[0].replace(",", "")
+        )
+        item_segunda_dosis = (
+            response.css(".cifra1.bg4.counter::text").getall()[0].replace(",", "")
+        )
+        item_fecha = (
+            response.css(
+                ".actualiza.d-flex.justify-content-between.flex-column.flex-md-row p::text"
+            )
+            .getall()[1]
+            .strip()
+        )
+        vacunasItem = VacunasItem(
+            primera_dosis=item_primera_dosis,
+            segunda_dosis=item_segunda_dosis,
+            fecha=item_fecha,
+        )
         yield vacunasItem
